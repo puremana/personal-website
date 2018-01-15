f1da94cb3d55d960d2fb4be828f6c6b20575ccf5
Jeremy Shaw <puremana@gmail.com>
2018-01-16 00:02:30 +1300

    Got a background I liked for my header, css animation.

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -19,7 +19,31 @@ class Home extends Component {
     return (
         <div class="home-page">
             <NavBar page="home"/>
+            <div class="header">
+                <h2>Jeremy Shaw Portfolio</h2>
+                <a class="button is-info is-large for-hire-button">
+                    For Hire
+                </a>
+            </div>
+            <div class="content-container cyan">
+                <div class="left-container">
+                    <h3>About</h3>
+                    <p></p>
+                </div>
+                <div class="right-container">
 
+                </div>
+                
+            </div>
+            <div class="content-container">
+                <h3>Education</h3>
+                <p>My relevant university courses</p>
+                <div class="course-container">
+                    <div class="course">
+                        <a href=""></a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
   }
