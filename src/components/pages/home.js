e481b41178a050aeae574d49522880e9a2b69f39
Jeremy Shaw <puremana@gmail.com>
2018-01-16 15:26:48 +1300

    Added color changes and all links in education container.

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -48,52 +48,52 @@ class Home extends Component {
                     <div class="level-block">
                         <h5>300 level</h5>
                         <div class="course">
-                            <a href="">COMP314 - Software Engineering Project</a>
+                            <a href="https://papers.waikato.ac.nz/papers/COMP314">COMP314 - Software Engineering Project</a>
                         </div>
                         <div class="course">
-                            <a href="">COMP336 – Graphics and Computer Games</a>
+                            <a href="https://papers.waikato.ac.nz/2017/COMP/COMP336">COMP336 – Graphics and Computer Games</a>
                         </div>
                         <div class="course">
-                            <a href="">COMP333 – Web Applications Development</a>
+                            <a href="https://papers.waikato.ac.nz/papers/2017/COMP333">COMP333 – Web Applications Development</a>
                         </div>
                         <div class="course">
-                            <a href="">COMP316 - Artificial Intelligence Techniques and Applications</a>
+                            <a href="https://papers.waikato.ac.nz/papers/COMP316">COMP316 - Artificial Intelligence Techniques and Applications</a>
                         </div>
                     </div>
                     <div class="level-block">
                         <h5>200 level</h5>
                         <div class="course">
-                            <a href="">COMP235 – Logic and Computation</a>
+                            <a href="https://papers.waikato.ac.nz/2017/COMP/COMP235">COMP235 – Logic and Computation</a>
                         </div>
                         <div class="course">
-                            <a href="">COMP233 – Internet Applications</a>
+                            <a href="https://papers.waikato.ac.nz/2017/COMP/COMP233">COMP233 – Internet Applications</a>
                         </div>
                         <div class="course">
-                            <a href="">COMP204 – Object Orientated Program Design</a>
+                            <a href="https://papers.waikato.ac.nz/2017/COMP/COMP204">COMP204 – Object Orientated Program Design</a>
                         </div>
                         <div class="course">
-                            <a href="">COMP202 – Computer Communications</a>
+                            <a href="https://papers.waikato.ac.nz/papers/2017/COMP202">COMP202 – Computer Communications</a>
                         </div>
                         <div class="course">
-                            <a href="">COMP200 – Computer Systems</a>
+                            <a href="https://papers.waikato.ac.nz/2017/COMP/COMP200">COMP200 – Computer Systems</a>
                         </div>
                         <div class="course">
-                            <a href="">COMP203 – Programming with Data Structures</a>
+                            <a href="https://papers.waikato.ac.nz/2017/COMP/COMP203">COMP203 – Programming with Data Structures</a>
                         </div>
                         <div class="course">
-                            <a href="">COMP224 – Visual Design for Interactive Media</a>
+                            <a href="https://papers.waikato.ac.nz/2017/COMP/COMP224">COMP224 – Visual Design for Interactive Media</a>
                         </div>
                     </div>
                     <div class="level-block">
                         <h5>100 level</h5>
                         <div class="course">
-                            <a href="">COMP103 – Introduction to Computer Science 1</a>
+                            <a href="https://papers.waikato.ac.nz/2017/COMP/COMP103">COMP103 – Introduction to Computer Science 1</a>
                         </div>
                         <div class="course">
-                            <a href="">COMP104 – Introduction to Computer Science 2</a>
+                            <a href="https://papers.waikato.ac.nz/2017/COMP/COMP104">COMP104 – Introduction to Computer Science 2</a>
                         </div>
                         <div class="course">
-                            <a href="">COMP126 – Computing Media</a>
+                            <a href="https://papers.waikato.ac.nz/2017/COMP/COMP126">COMP126 – Computing Media</a>
                         </div>
                     </div>
                 </div>
